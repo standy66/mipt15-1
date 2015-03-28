109f0c0096b4b3277bd628879db77ea261dc46d3
Andrew Stepanov <karn9050@gmail.com>
2015-03-28 09:01:40 +0300

    lab0 done

--- lab0/lab0.hs
+++ lab0/lab0.hs
@@ -6,11 +6,14 @@ import Data.Text.Encoding
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Char8 as C
 
-(email, name) = ("", encodeUtf8 "") -- адрес почты и фамилия с инициалами
+(email, name) = ("karn9050@gmail.com", encodeUtf8 "Степанов А.В.") -- адрес почты и фамилия с инициалами
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = product [1..n]
 
 pascal :: Int -> Int -> Int
-pascal c r = 1 -- а тут решение
-
+pascal m n = (factorial n) `quot` ((factorial $ (n - m)) * (factorial m))
 printIt :: Int -> C.ByteString
 printIt n = C.pack $ show $ [pascal y x | x <- [0..n], y <- [0..x]]
 
